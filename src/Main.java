342e5971be38f2202551da2c859ad6a9afbfa693
Cwou <atoskuba@gmail.com>
2022-09-15 13:41:11 +0200

    Bez powtorzen

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,12 @@ public class Main {
         System.out.println("Podaj 6 liczb");
         for(int i = 0; i < 6; i++){
             System.out.println("Liczba " + i);
-            wpisane.add(klawiatura.nextInt());
+            int wczytanawart = klawiatura.nextInt();
+            while(wpisane.contains(wczytanawart)){
+                System.out.println("Taka liczba już została podana");
+                wczytanawart = klawiatura.nextInt();
+            }
+            wpisane.add(wczytanawart);
         }
         System.out.println("Wpisane " + wpisane);
         }
