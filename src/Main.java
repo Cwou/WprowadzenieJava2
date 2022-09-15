99fe3d7757549307dc00bbecd43af91fea1ea1f0
Cwou <atoskuba@gmail.com>
2022-09-15 12:43:06 +0200

    Losowanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hellow");
+        //losowanie 6 liczb, wypisywanie ich a potem tej wylosowanej
+        System.out.println("Losowanie 6 liczb");
+
+        //tablica
+        int [] wylosowane = new int[6];
+
+        //tablice w Javie nie mogą mieć zmienianego rozmiaru podczas działań
+        //tablice w Javie przechowują typy proste lub złożone
+
+        for(int i = 0; i<wylosowane.length; i++){
+            wylosowane[i] = (int)(Math.random() * 100 + 1);
+        }
+        for(int wyloswartosc :wylosowane){
+            System.out.println(wyloswartosc + " ");
+        }
     }
 }
